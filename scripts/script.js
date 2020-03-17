220febcb22e882bb822860b67785239005e74134
DmytroS-7 <dm.webmas12@gmail.com>
2020-03-17 18:57:33 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,123 @@
+function displayData(clientsList = clients) {
+  const ul = document.querySelector("#clientsData");
+  clientsList.forEach(client => {
+    ul.appendChild(getLiElement(client));
+  });
+  sumAmount(clientsList);
+}
+
+function getLiElement(client) {
+  const newLi = document.createElement("li");
+  const avatar = document.createElement("img");
+  newLi.className = "media";
+
+  avatar.className = "mr-3 align-self-center";
+  avatar.setAttribute("src", client.avatar);
+
+  newLi.appendChild(avatar);
+  newLi.appendChild(createClientDescription(client));
+  return newLi;
+}
+
+function createClientDescription(client) {
+  const div = document.createElement("div");
+  div.className = "media-body";
+
+  const mailLink = document.createElement("a");
+  mailLink.setAttribute("href", `mailto:${client.email}`);
+  mailLink.innerHTML = client.email;
+  const textPart1 = document.createTextNode(
+    `${client.lastName} ${client.firstName} - `
+  );
+  const textPart2 = document.createTextNode(
+    ` ${client.gender} (${client.date} - ${client.amount})`
+  );
+  div.appendChild(textPart1);
+  div.appendChild(mailLink);
+  div.appendChild(textPart2);
+
+  return div;
+}
+
+function sortList(order) {
+  const sortedClients = clients.sort((lastClient, nextClient) => {
+    if (order == "ascending") {
+      return lastClient.lastName > nextClient.lastName ? 1 : -1;
+    } else {
+      return lastClient.lastName < nextClient.lastName ? 1 : -1;
+    }
+  });
+  refreshData(sortedClients);
+}
+
+function refreshData(updatedClients) {
+  clearList();
+  displayData(updatedClients);
+}
+
+function clearList() {
+  const ul = document.querySelector("#clientsData");
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+}
+
+function filterList() {
+  const filterString = document
+    .querySelector("#filterInput")
+    .value.toLowerCase()
+    .trim();
+  if (filterString) {
+    const filteredClients = clients.filter(client => {
+      return (
+        client.firstName.toLowerCase().includes(filterString) ||
+        client.lastName.toLowerCase().includes(filterString) ||
+        client.email.toLowerCase().includes(filterString) ||
+        client.date.toLowerCase().includes(filterString)
+      );
+    });
+    refreshData(filteredClients);
+    filteredClients.length === 0
+      ? showResultListOrNotFound("showNotFound")
+      : showResultListOrNotFound();
+    //   showNotFoundSection()
+    //   : showResultListSection();
+  } else {
+    refreshData(clients);
+    // showResultListSection();
+    showResultListOrNotFound();
+  }
+}
+
+function sumAmount(clientsList = clients) {
+  const total = clientsList.reduce((amount, client) => {
+    return amount + removeCurrencyFromAmount(client.amount);
+  }, 0);
+  document.querySelectorAll(".totalAmountContainer").forEach(element => {
+    element.innerHTML = total.toFixed(2);
+  });
+}
+
+function removeCurrencyFromAmount(amount) {
+  return Number(amount.slice(1));
+}
+
+// function showNotFoundSection() {
+//   document.querySelector(".resultList").style.display = "none";
+//   document.querySelector(".notFound").style.display = "block";
+// }
+
+// function showResultListSection() {
+//   document.querySelector(".resultList").style.display = "block";
+//   document.querySelector(".notFound").style.display = "none";
+// }
+
+function showResultListOrNotFound(param = "showResultList") {
+  if (param == "showResultList") {
+    document.querySelector(".resultList").style.display = "block";
+    document.querySelector(".notFound").style.display = "none";
+  } else {
+    document.querySelector(".resultList").style.display = "none";
+    document.querySelector(".notFound").style.display = "block";
+  }
+}
